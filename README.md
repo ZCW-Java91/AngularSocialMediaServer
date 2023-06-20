bebae2700acbf452a8ee634dce6e84a8c3bcacf0
KrYounger <35814586+xt0fer@users.noreply.github.com>
2023-06-20 10:50:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Angular Social Media Server
+
+This server is meant to be used in conjunction with the Angular Social Media Client lab: AngularSocialMedia
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO user (id, username) VALUES
+  (1, 'me'),
+  (2, 'domi'),
+  (3, 'leon'),
+  (4, 'tariq');
+
+INSERT INTO post (id, content, user_id) VALUES
+  (1, 'I wonder what new donuts they have downtown', 2),
+  (2, 'Is it 4:30 yet?', 3),
+  (3, 'Why is Steven late again?', 4);
